4f86caa0005c8de7d87a6bc942357e7a3f0e210c
zhiyongli <zhiyongli@microsoft.com>
2025-12-08 12:54:31 +0800

    remove init

--- web/src/main/java/com/microsoft/migration/assets/model/ImageMetadata.java
+++ web/src/main/java/com/microsoft/migration/assets/model/ImageMetadata.java
@@ -1,6 +1,6 @@
 package com.microsoft.migration.assets.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- web/src/main/java/com/microsoft/migration/assets/service/LocalFileStorageService.java
+++ web/src/main/java/com/microsoft/migration/assets/service/LocalFileStorageService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
