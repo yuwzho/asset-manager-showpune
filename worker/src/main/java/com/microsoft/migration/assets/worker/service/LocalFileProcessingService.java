583932f064445c4f480e5ee8830f6e86cb162e84
yuwzho <yuwzho@microsoft.com>
2026-02-25 17:16:43 +0800

    Upgrade Java to 21 and Spring Boot to 3.4.2

--- worker/src/main/java/com/microsoft/migration/assets/worker/service/LocalFileProcessingService.java
+++ worker/src/main/java/com/microsoft/migration/assets/worker/service/LocalFileProcessingService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import jakarta.annotation.PostConstruct;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +23,7 @@ public class LocalFileProcessingService extends AbstractFileProcessingService {
     
     @PostConstruct
     public void init() throws Exception {
-        rootLocation = Paths.get(storageDirectory).toAbsolutePath().normalize();
+        rootLocation = Path.of(storageDirectory).toAbsolutePath().normalize();
         logger.info("Local storage directory: {}", rootLocation);
         
         if (!Files.exists(rootLocation)) {
